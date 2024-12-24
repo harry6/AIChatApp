b778d6327e3ba82c206ea8f4feca9c4f367c02ad
harry <harry@macmini.com>
2024-12-24 08:40:06 +0800

    use KeychainHelper utility to save sensitive info such as API key.

--- AIChatAppSwift/AIApp/Models/Settings.swift
+++ AIChatAppSwift/AIApp/Models/Settings.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 class Settings: ObservableObject {
+    private let keychainHelper = KeychainHelper.standard
+    private let openAIKeyService = "com.TEST.AIApp.openai"
+    private let deepSeekKeyService = "com.TEST.AIApp.deepseek"
+    private let accountName = "apikey"
+    
     @Published var selectedService: AIServiceType {
         didSet {
             UserDefaults.standard.set(selectedService.rawValue, forKey: "selectedService")
@@ -9,19 +14,36 @@ class Settings: ObservableObject {
     
     @Published var openAIKey: String {
         didSet {
-            UserDefaults.standard.set(openAIKey, forKey: "openAIKey")
+            try? keychainHelper.save(openAIKey.data(using: .utf8) ?? Data(),
+                                   service: openAIKeyService,
+                                   account: accountName)
         }
     }
     
     @Published var deepSeekKey: String {
         didSet {
-            UserDefaults.standard.set(deepSeekKey, forKey: "deepSeekKey")
+            try? keychainHelper.save(deepSeekKey.data(using: .utf8) ?? Data(),
+                                   service: deepSeekKeyService,
+                                   account: accountName)
         }
     }
     
     init() {
         self.selectedService = AIServiceType(rawValue: UserDefaults.standard.string(forKey: "selectedService") ?? "") ?? .openAI
-        self.openAIKey = UserDefaults.standard.string(forKey: "openAIKey") ?? ""
-        self.deepSeekKey = UserDefaults.standard.string(forKey: "deepSeekKey") ?? ""
+        
+        // Load API keys from Keychain
+        if let openAIData = keychainHelper.read(service: openAIKeyService, account: accountName),
+           let openAIString = String(data: openAIData, encoding: .utf8) {
+            self.openAIKey = openAIString
+        } else {
+            self.openAIKey = ""
+        }
+        
+        if let deepSeekData = keychainHelper.read(service: deepSeekKeyService, account: accountName),
+           let deepSeekString = String(data: deepSeekData, encoding: .utf8) {
+            self.deepSeekKey = deepSeekString
+        } else {
+            self.deepSeekKey = ""
+        }
     }
 }
---
+++ AIChatAppSwift/AIApp/Utilities/KeychainHelper.swift
@@ -0,0 +1,74 @@
+import Foundation
+import Security
+
+enum KeychainError: Error {
+    case duplicateEntry
+    case unknown(OSStatus)
+    case itemNotFound
+}
+
+class KeychainHelper {
+    static let standard = KeychainHelper()
+    private init() {}
+    
+    func save(_ data: Data, service: String, account: String) throws {
+        let query: [String: AnyObject] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject,
+            kSecValueData as String: data as AnyObject
+        ]
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        
+        if status == errSecDuplicateItem {
+            try update(data, service: service, account: account)
+        } else if status != errSecSuccess {
+            throw KeychainError.unknown(status)
+        }
+    }
+    
+    func update(_ data: Data, service: String, account: String) throws {
+        let query: [String: AnyObject] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject
+        ]
+        
+        let attributes: [String: AnyObject] = [
+            kSecValueData as String: data as AnyObject
+        ]
+        
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        guard status != errSecItemNotFound else { throw KeychainError.itemNotFound }
+        guard status == errSecSuccess else { throw KeychainError.unknown(status) }
+    }
+    
+    func read(service: String, account: String) -> Data? {
+        let query: [String: AnyObject] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject,
+            kSecReturnData as String: kCFBooleanTrue,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        return status == errSecSuccess ? result as? Data : nil
+    }
+    
+    func delete(service: String, account: String) throws {
+        let query: [String: AnyObject] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw KeychainError.unknown(status)
+        }
+    }
+} 
